418bd466c4b13c7ba372846e31ccc4bd76dc150c
Anish <anish.munirathinam@gmail.com>
2022-05-22 17:54:31 +0530

    🎉 [CS193P-1] Inital commit

---
+++ Emoji Match/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emoji Match
+//
+//  Created by Munirathinam, Anish | RIEPL on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emoji Match/Emoji_MatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_MatchApp.swift
+//  Emoji Match
+//
+//  Created by Munirathinam, Anish | RIEPL on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_MatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
